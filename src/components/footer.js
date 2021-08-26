8fc293ab8cf97434d7bc93aa6bb9028970358f6b
Akshat6-web <akshatslnk121@gmail.com>
2021-08-26 12:01:13 +0530

    First commit

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,14 +1,21 @@
 import React from "react";
 
 export default function Footer() {
-	return (
-		<div
-			style={{
-				width: "100%",
-				height: 200,
-				backgroundColor: "grey",
-				marginTop: 50,
-			}}
-		></div>
-	);
+  return (
+    <div
+      style={{
+        width: "100%",
+
+        backgroundColor: "#4791db",
+
+        textAlign: "center",
+        height: "70px",
+        marginTop: "25%",
+        fontSize: "20px",
+      }}
+    >
+      {" "}
+      <p>&copy; All Rights Reserved</p>
+    </div>
+  );
 }
